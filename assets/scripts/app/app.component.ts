69b4a8adc47ade4e348c6e15787c19525a5dd40f
Corey Ricketts <cricketts@ncl.com>
2017-02-20 14:00:59 -0500

    dynamic loading is working

--- assets/scripts/app/app.component.ts
+++ assets/scripts/app/app.component.ts
@@ -3,7 +3,7 @@ import { DataService } from "./app.service";
 
 @Component({
     selector: "app",
-    template: "<div>I can haz decorator?</div>",
+    template: "<div>I can haz decorator? k thx bai</div>",
     providers: [DataService]
 })
 export class AppComponent {
